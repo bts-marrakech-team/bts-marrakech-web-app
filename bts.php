340ac8f82a0297e5dafba8c19ae982246cab4ee3
FatimezzahraElAaroubi <FATIFATIMA.ARROUBI@gmail.com>
2019-03-01 13:52:26 +0100

    Initial commit

---
+++ bts.php
@@ -0,0 +1,330 @@
+
+<!DOCTYPE>
+<html>
+<head>
+  <link rel="stylesheet" href="assets/css/font-awesome.css">
+<style TYPE="text/css" >
+BODY {
+}
+a:active, a:link, a:visited {  color: #336699; text-decoration: none; font-weight: bold; font-family: Arial; font-size: 8pt;}
+a:hover {  color: #db9504; text-decoration: none}
+td {
+	font-family: Arial;
+	font-size: 7pt;
+	font-style: normal;
+	color: #000000
+} 
+input { color: #000000; border-bottom-width: 1px; border-left-width: 1px; border-right-width: 1px; border-top-width: 1px; border-bottom-color: #000000; border-left-color: #000000; border-right-color: #000000; border-top-color: #000000; font-family: 'verdana', 'arial', 'helvetical', 'geneva', sans-serif; font-size: 11px; background: #D0DDEA; }
+textarea { color: #000000; border-bottom-width: 1px; border-left-width: 1px; border-right-width: 1px; border-top-width: 1px; border-bottom-color: #000000; border-left-color: #000000; border-right-color: #000000; border-top-color: #000000; font-family: 'verdana', 'arial', 'helvetical', 'geneva', sans-serif; font-size: 10px; background: #D0DDEA; }
+select { color: #000000; border-bottom-width: 1px; border-left-width: 1px; border-right-width: 1px; border-top-width: 1px; border-bottom-color: #000000; border-left-color: #000000; border-right-color: #000000; border-top-color: #000000; font-family: 'verdana', 'arial', 'helvetical', 'geneva', sans-serif; font-size: 10px; background: #D0DDEA; }
+a.menu:active, a.menu:link, a.menu:visited {
+	color: #D5EAFF;
+	text-decoration: none;
+	font-weight: bold;
+	font-family: Arial;
+	font-size: 7pt;
+}
+a.menu:hover {  color: #FFFFFF; text-decoration: none} 
+.style2 {
+	color: #db9504;
+	text-align: center;
+}
+.style3 {color: #ECA20D}
+.style6 {color: #FFFFFF}
+.Style7 {font-size: 7pt}
+.Style8 {
+	color: #990000;
+	font-size: 16px;
+	font-weight: bold;
+	font-family: "Times New Roman", Times, serif;
+}
+.Style9 {font-size: 9pt}
+.Style13 {font-size: 10px}
+.Style14 {font-size: 11px}
+.Style15 {font-size: 8px}
+.Style16 {font-size: 7px}
+.Style17 {font-size: 16px}
+.Style18 {font-family: "Times New Roman", Times, serif}
+.Style20 {color: #000000}
+.Style21 {color: #990000}
+.comp {
+	color: #ECA13A;
+}
+.ccccc {
+	font-weight: bold;
+}
+.comdpc {
+	color: #F90;
+}
+.ttt {
+	text-align: center;
+}
+-->
+</style>
+<script language="javascript">
+function resizeframe() {
+	var a = document.getElementById('conteneurframe').offsetHeight;
+	document.getElementById('ajouter').style.height = a + 'px'
+}
+</script>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta http-equiv="Content-Language" content="fr">
+<TITLE>Brevet de Technicien Superieur </TITLE>
+<meta name="Author" content="Brevet de Technicien Superieur "><meta name="Identifier-URL" content="Brevet de Technicien Superieur ">
+<meta name="Copyright" content="© BTS_Marrakech">
+  
+</head>
+<body topmargin="0" leftmargin="0" background="img/fond.jpg" marginwidth="0" marginheight="0"><table width="960" border="0" cellspacing="1" cellpadding="0" align="center" background="img/fond.jpg"><tr><td width="100%" height="818"><table width="100%" border="0" cellspacing="0" cellpadding="0" height="185" background="img/marcheverte.jpg">
+  
+        <tr>
+          <td width="100%" height="26" colspan="2" align="right" background="img/bar.jpg"><table width="527" border="0" align="left">
+            <tr>
+              <td width="521"><span class="Style13"><span class="Style14"><span class="Style15"><span class="Style16"><span class="Style17"><span class="Style18"><span class="Style20"><span class="Style21"><a href="presentation.php" target="index"><b>&nbsp;Pr&egrave;sentation</b> </a><b> &nbsp;-&nbsp;</b></span></span></span></span></span></span></span></span><span class="Style8">&nbsp;<a href="contact.php" target="index"><b>Nous contacter&nbsp; </b></a> - <a href="plan.php" target="index"><b>&nbsp;Plan d'acc&egrave;s</b> </a>- <a href="actu.php" target="index"><b>&nbsp;Page d'accueil</b></a>&nbsp;</span><span class="Style9"><font color="#999999">&nbsp;&nbsp;</font></span><font color="#999999">&nbsp;</font></td>
+            </tr>
+          </table>
+            <table width="200" border="0">
+            <tr>
+              <td width="15"><span class="style6"></span></td>
+              <td width="175"><span class="style6"><strong>
+                  
+</strong></span><strong><span class="style6" style="font-size:9pt;"><b>              
+              </b></span></strong></td>
+            </tr>
+          </table></td>
+        </tr>
+        
+        
+        <tr>
+          <td height="153" colspan="2"><table width="100%" height="162" border="0" bgcolor="white">
+              
+<td height="118" colspan="3" bgcolor="#FFFFFF" width="100%"><img src="btsHassan2.jpg" width="100%" height="118" border="0"></td>
+              
+                            <tr>
+                                <td width="562" height="36" background="img/ban4.png">
+
+                                    <p><span class="mise_en_avant"><marquee behavior="scroll" direction="left" scrollamount="2" scrolldelay="0">
+<font color="blue"><b><span style="font-size:10pt;">Bienvenue sur le site web de Brevet De Technicien Superieur Marrakech Lyc&egrave;e Hassan 2</span></b><br />
+</font></marquee></span></p>
+                                </td>
+                                <td width="372" valign="middle" height="36">
+                                    <p align="center"><b><font color="#00366D"><span style="font-size:9pt;">Rechercher sur ce site:&nbsp;&nbsp;</span></font></b><b><font color="#00366D"><span style="font-size:8pt;">
+                <input type="text" name="textfield" id="textfield">
+  &nbsp;&nbsp;            
+               </span></font></b><a href="#" ><img id="search_img" src="img/wrd_img_69_1.gif" alt="" title="" border="0"></a></p>
+                                </td>                
+                
+                            </tr>
+              
+          </table>          </td>
+        </tr>
+        
+</table>
+<table width="100%" border="0" cellspacing="1" cellpadding="0" bgcolor="#000000"><tr>
+          <td bgcolor="#EFF3F8" width="133" valign="top" background="img/fondmenu.png" height="612"> 
+            <table width="134" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td bgcolor="#FFFFFF">
+                	<div align="center" class="style2">
+                		<!-- <i class="fa fa-star"></i> -->
+                		<ion-icon name="star" size="small"></ion-icon>
+                		<b><font color="#ECA20D"><span style="font-size:9pt;">Pr&egrave;sentation</span></font></b>
+                	</div>
+                </td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png">&nbsp;</td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png"><font color="#FFFFFF"> - <a href="mot.php" class="menu" target="index">Mot du directeur</a></font></td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png"><font color="#FFFFFF"> - <a href="presentation.php" class="menu" target="index">BTS en bref</a></font></td>
+              </tr>
+              <tr>
+                <td background="fondmenu.png"><font color="#FFFFFF"> - <a href="BTS_chiffres.php" class="menu" target="index">BTS en chiffres</a></font></td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png">&nbsp;</td>
+              </tr>
+            </table>
+            
+            <table width="134" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td bgcolor="#FFFFFF" background="img/etoiles.gif"><div align="center" class="style2"><b>&nbsp;<font color="#ECA20D"><span class="style3" style="font-size:9pt;">Formations</span></font></b></div></td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png">&nbsp;</td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png"><font color="#FFFFFF"> - <a href="fil.php" target="index" class="menu Style7">En Developpement des Systemes d'information</a></font></td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png"><font color="#FFFFFF"> - <a href="master.php" class="menu" target="index">En Batiment</a></font></td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png">&nbsp;</td>
+              </tr>
+            </table>
+      
+            <table width="134" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td bgcolor="#FFFFFF" background="img/etoiles.gif"><div align="center" class="style2"><b>&nbsp;&nbsp;<font color="#ECA20D"><span class="style3" style="font-size:9pt;">Recherche</span></font></b></div></td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png">&nbsp;</td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png"><font color="#FFFFFF"> - <a href="https://massar.men.gov.ma/Account" class="menu" target="index">Les Notes</a></font></td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png"><font color="#FFFFFF"> - <a href=".php" class="menu" target="index">L'emploi du temps </a></font></td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png"><font color="#FFFFFF"> - <a href="ced.php" class="menu" target="index">Cours et Exercice</a></font></td>
+              </tr>
+              
+              <tr>
+                <td background="img/fondmenu.png">&nbsp;</td>
+              </tr>
+            </table>
+            <table width="134" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td bgcolor="#FFFFFF" background="img/etoiles.gif"><div align="center" class="style2"><b>&nbsp;&nbsp;<font color="#ECA20D"><span class="style3" style="font-size:9pt;">Activit&egrave;s</span></font></b></div></td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png">&nbsp;</td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png"><font color="#FFFFFF"> - <a href="integration.php" class="menu" target="index">F&echapo;te d'integration</a></font></td>
+              </tr>
+  <td valign="middle" background="img/fondmenu.png"> &nbsp; &nbsp;&nbsp;&nbsp;<img src="qualite_iso.gif" width="67" height="77" border="0" align="middle"></td>
+            </table>
+          </td>
+          <td bgcolor="#DDEEFF" width="767" valign="top" background="img/bannhaut2.png" height="612"><table width="100%" border="0" cellspacing="0" cellpadding="1" valign="middle" align="center"></table>
+            <br>
+            
+            <table width="667" height="571" border="0" align="center" bgcolor="#999999">
+<tr>
+                <td width="685" height="567" valign="top" nowrap background="backall1" bgcolor="#cccccc"><iframe align="middle" name="index" frameborder="0" width="100%" height="100%" id="test" src="actu.php" scrolling="auto"> </iframe></td>
+</tr></table>
+<table width="692" border="0" cellspacing="0" align="center" bgcolor="#999999">
+<tr>
+                                <td width="664" height="72" background="img/contact23.jpg" bgcolor="#F7F9FB">
+                    <div align="center"><div class="Style44" align="center">                                                                                                                                                                                                                                                                        <address>
+<font color="maroon"><span style="font-size:10pt;"><b></b></span></font><b><font color="maroon"><span style="font-size:9pt;">&nbsp;</span></font></b><a href="contact.php" target="index"><b><font color="maroon"><span style="font-size:10pt;">Brevet de technicien Superieur Lyc&egrave;e Hassan2 </span></font></b></a></address>
+                                            <address>
+<a href="contact.php" target="index"><b><font color="#333333"><span style="font-size:10pt;">Ly. Hassan II - Quartier Guéliz - Marrakech</span></font></b></a><span style="font-size:10pt;"></span></address>
+                                            <address>
+<a href="contact.php"><b><font color="#333333"><span style="font-size:10pt;">Tél : (+212) 524 430 792&nbsp;</span></font></b><font color="#333333"><span style="font-size:10pt;">&nbsp;</span></font></a><a href="contact.html"><font color="#333333"><span style="font-size:10pt;"></span></font></a><font color="#333333"><span style="font-size:10pt;"></span></font><font color="#333333"><span style="font-size:10pt;"></span></font><span style="font-size:10pt;"></span>
+                      </address>
+</div>
+	          <div class="Style9 Style13 Style45" align="center">                                                                                         </div>
+			          <div class="Style9 Style13 Style45" align="center">                                                                                                                                    </div>
+</div>
+                </td>
+              </tr>
+              
+</table>
+          </td>
+          <td bgcolor="#EFF3F8" width="133" height="612" valign="top" background="img/fondmenu.png">
+            <table width="134" border="0" cellspacing="0" cellpadding="0" bgcolor="white">
+              <tr>
+                <td bgcolor="#FFFFFF" background="img/etoiles.gif"><div align="center" class="style2"><b><font color="#ECA20D"><span class="style3" style="font-size:9pt;">Matiers</span></font></b></div></td>
+              </tr>
+               <tr>
+                <td background="img/fondmenu.png"><font color="#FFFFFF"> - <a href="DSI.php" class="menu" target="index">Matiers de DSI</a></font></td>
+              </tr>
+               <tr>
+                <td background="img/fondmenu.png"><font color="#FFFFFF"> - <a href="bat.php" class="menu" target="index">matiers de batiment</a></font></td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png">&nbsp;</td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png"><p><font color="#FFFFFF"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</font><a href="matiers.php" target="new"><img src="img/enveloppe.gif" alt="Matiers" width="37" height="28" border="0"></a><font color="#FFFFFF"> </font></p></td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png">&nbsp;</td>
+              </tr>
+            </table>
+            <table width="134" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td bgcolor="#FFFFFF" background="img/etoiles.gif"><div align="center" class="style2"><b><font color="#ECA20D"><span class="style3" style="font-size:9pt;">E-Learning</span></font></b></div></td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png">&nbsp;</td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png"><font color="#FFFFFF"> - <a href="lien de l'app" class="menu">Cours et exercices</a></font></td>
+              </tr>
+			  <tr>
+                <td background="img/fondmenu.png">&nbsp;</td>
+              </tr>
+			  <tr>
+                                <td background="img/fondmenu.png"><p><font color="#FFFFFF"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</font><a href=""></a><font color="#FFFFFF"> </font></p></td>
+              </tr>
+			   <tr>
+                <td background="img/fondmenu.png">&nbsp;</td>
+              </tr>
+            </table>
+            <table width="134" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td bgcolor="#FFFFFF" background="img/etoiles.gif"><div align="center" class="style2 style2"><b><font color="#ECA20D"><span style="font-size:9pt;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Espace Etudiant</span></font></b></div></td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png">&nbsp;</td>
+              </tr>
+                 <tr>
+                <td background="img/fondmenu.png"><font color="#FFFFFF"> - <a href="futur_etudiant.php" class="menu" target="index"> Guide  de l'étudiant</a></font></td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png"><font color="#FFFFFF"> - <a href="futur_etudiant.php" class="menu" target="index">Futur Etudiant</a></font></td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png"><font color="#FFFFFF"> - <a href="tele_etudiant.php" class="menu" target="index">Téléchargement</a></font></td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png">&nbsp;</td>
+              </tr>
+            </table>
+            <table width="134" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td background="img/fondmenu.png">&nbsp;</td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png">&nbsp;</td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png">&nbsp;</td>
+              </tr>
+            </table>
+            <table width="134" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td bgcolor="#FFFFFF" background="img/etoiles.gif"><div align="center" class="style2 style2"><b><font color="#ECA20D"><span style="font-size:9pt;"> </span></font></b></div></td>
+              </tr>
+              <tr>
+                
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png"><p><span class="ccccc">
+                
+                  </span>
+                    </p>
+                  <p>
+                    
+                  
+                  
+                  </p>
+                </p></td>
+              </tr>
+              <tr>
+                <td background="img/fondmenu.png"></td>
+              </tr>
+            </table>
+          </td>
+      </tr></table><table width="100%" border="0" cellspacing="1" cellpadding="0" bgcolor="#000000"><tr><td height="12"  background="img/bannhaut2.png">
+            <div align="center"><b><font color="#FFFFFF">© Copyright 2019 - BTS</font></b></div>
+          </td></tr></table></td></tr></table>
+
+          <script src="https://unpkg.com/ionicons@4.4.4/dist/ionicons.js"></script>
+</body></html>
